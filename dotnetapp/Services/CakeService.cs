c7f0c6cb52ea46380df048e250a0a1bc02d36af4
abhinavpant <abhinavpant53@gmail.com>
2025-04-16 10:23:42 +0000

    CakeService 1604:3.53pm

---
+++ dotnetapp/Services/CakeService.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Data;
+
+namespace dotnetapp.Services
+{
+    public class CakeService{
+        private readonly ApplicationDbContext _context;
+        public CakeService(ApplicationDbContext context){
+            _context = context;
+        }
+        public async Task<IEnumerable<Cake>> GetAllCakes(){
+            return await _context.Cakes.ToListAsync();
+        }
+        public async Task<Cake> GetCakeById(int cakeId){
+            return await _context.Cakes.FindAsync(cakeId);
+        }
+        public async Task<bool> AddCake(Cake cake){
+            var exists = await _context.Cakes.FirstOrDefaultAsync(c=>c.Name==cake.Name);
+            if(exists!= null){
+                return false;
+            }
+            _context.Cakes.Add(cake);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> UpdateCake(int cakeId,Cake cake){
+            var exists1 = await _context.Cakes.FindAsync(cakeId);
+            if(exists1==null){
+                return false;
+            }
+            var exists2 = await _context.Cakes.FirstOrDefaultAsync(c=>c.Category ==cake.Category && c.CakeId !=cakeId);
+            if(exists2 != null){
+                return false;
+            }
+
+            exists1.Name=cake.Name;
+            exists1.Category = cake.Category;
+            exists1.Price = cake.Price;
+            exists1.Quantity = cake.Quantity;
+            exists1.CakeImage = cake.CakeImage;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteCake(int cakeId){
+         var delcake = await _context.Cakes.FindAsync(cakeId);
+         if(delcake==null){
+            return false;
+         }
+         _context.Cakes.Remove(delcake);
+         await _context.SaveChangesAsync();
+         return true;
+        }
+    }
+}
