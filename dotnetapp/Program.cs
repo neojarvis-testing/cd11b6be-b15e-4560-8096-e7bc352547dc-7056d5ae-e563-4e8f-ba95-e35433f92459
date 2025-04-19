b1cad32f8b36a5c92a6bd00c3d7956b1d21cfc17
Shivani2569 <shivanivodnala01@gmail.com>
2025-04-19 10:43:23 +0000

    app

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -13,11 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-<<<<<<< HEAD
-        policy.WithOrigins("https://8081-cccfccfacbdcbaaefeacaedadaeeefbaef.premiumproject.examly.io") // Replace with your frontend URL
-=======
-        policy.WithOrigins("https://8081-ebdedcfaaebbaaefeacaedadaeeefbaef.premiumproject.examly.io") // Replace with your frontend URL
->>>>>>> 72d3b3aa1c10c50564d8cc49ee268d6cedcbb4bc
+        policy.WithOrigins("https://8081-cccfccfacbdcbaaefeacaedadaeeefbaef.premiumproject.examly.io/") // Replace with your frontend URL
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
